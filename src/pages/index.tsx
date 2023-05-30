6c599ff3aac7f07955f4a3b6cb6602e7ea256af4
Tanay Patil <tanaypatil2003@gmail.com>
2023-05-30 14:21:50 -0700

    Notification button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,6 +47,25 @@ export default function Home ({}: HomePageProps) {
   return (
     <>
       <header className='header flex justify-end'>
+        {isConnected && (
+          <button className='mr-4 notif-btn'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
+              />
+            </svg>
+            <span className='notif-dot bg-red-500 rounded-full w-1 h-1'>1</span>
+          </button>
+        )}
         <ConnectKitButton />
       </header>
       <main>
@@ -105,19 +124,19 @@ export default function Home ({}: HomePageProps) {
               </label>
               <div className='flex gap-4'>
                 {cooldown === null ? (
-                  <button className='flex-1 rounded-md mt-1 h-12 bg-blue-500'>
+                  <button className='flex-1 rounded-md mt-1 h-12 bg-blue-500 text-white'>
                     Bid
                   </button>
                 ) : (
                   <button
-                    className='flex-1 rounded-md mt-1 h-12 dark:bg-gray-800 dark:text-gray-400'
+                    className='flex-1 rounded-md mt-1 h-12 bg-gray-100 text-gray-400 dark:bg-gray-800 dark:text-gray-400'
                     disabled
                   >
                     Wait {time(cooldown - now)} to bid again
                   </button>
                 )}
                 <button
-                  className='flex-1 rounded-md mt-1 h-12 bg-orange-500'
+                  className='flex-1 rounded-md mt-1 h-12 bg-orange-500 text-white'
                   type='button'
                 >
                   Settle
